baf2b4be6df5ce9a9e6c148f46c45cbc5b395a2c
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:30:13 -0300

    Update contas-pagas.js

--- js/modules/contas-pagas.js
+++ js/modules/contas-pagas.js
@@ -1,1 +1,25 @@
+window.contasPagasModule = {
 
+  async carregarContasPagas() {
+    const { mes, ano } = utils.getMesAno();
+
+    const data = await api.restGet("contas_pagar",
+      `select=*&mes=eq.${mes}&ano=eq.${ano}&status=eq.pago`
+    );
+
+    const tbody = tabelaContasPagas;
+
+    tbody.innerHTML = data.map(i => `
+      <tr>
+        <td>${i.fornecedor}</td>
+        <td>${i.descricao}</td>
+        <td>${i.categoria}</td>
+        <td>${i.documento}</td>
+        <td>${utils.moeda(i.valor)}</td>
+        <td>${i.data_pagamento}</td>
+        <td>${utils.moeda(i.valor)}</td>
+      </tr>
+    `).join("");
+  }
+
+};
