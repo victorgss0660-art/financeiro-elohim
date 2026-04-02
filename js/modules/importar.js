07f1a03628b53432314186ef3a62a9064eb6d25c
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:31:13 -0300

    Update importar.js

--- js/modules/importar.js
+++ js/modules/importar.js
@@ -1,1 +1,21 @@
+window.importarModule = {
 
+  handleFile(e) {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (evt) => {
+      const wb = XLSX.read(evt.target.result, { type: "binary" });
+      const ws = wb.Sheets[wb.SheetNames[0]];
+      const data = XLSX.utils.sheet_to_json(ws);
+
+      console.log("IMPORTADO:", data);
+      utils.setAppMsg("Planilha carregada (console)", "ok");
+    };
+
+    reader.readAsBinaryString(file);
+  }
+
+};
