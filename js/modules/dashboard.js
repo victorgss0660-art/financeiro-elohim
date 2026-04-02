d79331101f25d48e9d347de7281c1ddaa94c1e0c
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:32:10 -0300

    Update dashboard.js

--- js/modules/dashboard.js
+++ js/modules/dashboard.js
@@ -1,1 +1,10 @@
+window.dashboardModule = {
 
+  async carregarDashboard() {
+    document.getElementById("fat").textContent = "R$ 0,00";
+    document.getElementById("gas").textContent = "R$ 0,00";
+    document.getElementById("saldo").textContent = "R$ 0,00";
+    document.getElementById("metaAtingida").textContent = "0%";
+  }
+
+};
