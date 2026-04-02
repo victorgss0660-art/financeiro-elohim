fb10c3a044283aa03ca6c8637524b63a6139a22d
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:31:32 -0300

    Update resumo.js

--- js/modules/resumo.js
+++ js/modules/resumo.js
@@ -1,1 +1,11 @@
+window.resumoModule = {
 
+  async carregarResumoAnual() {
+    const tbody = document.getElementById("tabelaResumoAnual");
+
+    tbody.innerHTML = `
+      <tr><td>Janeiro</td><td>R$ 0</td><td>R$ 0</td><td>R$ 0</td></tr>
+    `;
+  }
+
+};
