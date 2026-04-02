5e7a5d9c2ffe1e95023735bdc575a4701f27671f
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:31:52 -0300

    Update planejamento.js

--- js/modules/planejamento.js
+++ js/modules/planejamento.js
@@ -1,1 +1,11 @@
+window.planejamentoModule = {
 
+  async carregarPlanejamento() {
+    const tbody = document.getElementById("tabelaPlanejamento");
+
+    tbody.innerHTML = `
+      <tr><td>Semana 1</td><td>R$ 0</td><td>R$ 0</td><td>R$ 0</td></tr>
+    `;
+  }
+
+};
