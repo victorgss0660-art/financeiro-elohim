ebc3de9d6d7f6810d79d87ee492b23280a5093a7
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:30:33 -0300

    Update faturamento.js

--- js/modules/faturamento.js
+++ js/modules/faturamento.js
@@ -1,1 +1,15 @@
+window.faturamentoModule = {
 
+  async salvarFaturamento() {
+    const { mes, ano } = utils.getMesAno();
+
+    await api.restInsert("faturamento", [{
+      valor: Number(faturamentoInput.value),
+      mes,
+      ano
+    }]);
+
+    utils.setAppMsg("Faturamento salvo", "ok");
+  }
+
+};
