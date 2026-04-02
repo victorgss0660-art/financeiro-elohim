259cd053d248a8e3a76ac858b96b1e341655884b
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 14:52:52 -0300

    Update contas-recebidas.js

--- js/modules/contas-recebidas.js
+++ js/modules/contas-recebidas.js
@@ -1,31 +1,37 @@
 window.contasRecebidasModule = {
-
   async carregarContasRecebidas() {
-    const { mes, ano } = utils.getMesAno();
+    try {
+      const { mes, ano } = utils.getMesAno();
 
-    const data = await api.restGet(
-      "contas_receber",
-      `select=*&mes=eq.${mes}&ano=eq.${ano}&status=eq.recebido`
-    );
+      const data = await api.restGet(
+        "contas_receber",
+        `select=*&mes=eq.${encodeURIComponent(mes)}&ano=eq.${encodeURIComponent(ano)}&status=eq.recebido&order=data_recebimento.desc`
+      );
 
-    const tbody = document.getElementById("tabelaContasRecebidas");
+      const tbody = document.getElementById("tabelaContasRecebidas");
 
-    if (!data.length) {
-      tbody.innerHTML = `<tr><td colspan="7" class="muted">Nenhuma conta recebida.</td></tr>`;
-      return;
+      if (!data.length) {
+        tbody.innerHTML = `
+          <tr>
+            <td colspan="7" class="muted">Nenhuma conta recebida.</td>
+          </tr>
+        `;
+        return;
+      }
+
+      tbody.innerHTML = data.map(item => `
+        <tr>
+          <td>${item.cliente || "-"}</td>
+          <td>${item.descricao || "-"}</td>
+          <td>${item.documento || "-"}</td>
+          <td>${item.categoria || "-"}</td>
+          <td>${utils.moeda(item.valor || 0)}</td>
+          <td>${item.data_recebimento || "-"}</td>
+          <td>${utils.moeda(item.valor || 0)}</td>
+        </tr>
+      `).join("");
+    } catch (e) {
+      utils.setAppMsg("Erro ao carregar contas recebidas: " + e.message, "err");
     }
-
-    tbody.innerHTML = data.map(i => `
-      <tr>
-        <td>${i.cliente}</td>
-        <td>${i.descricao}</td>
-        <td>${i.documento}</td>
-        <td>${i.categoria}</td>
-        <td>${utils.moeda(i.valor)}</td>
-        <td>${i.data_recebimento}</td>
-        <td>${utils.moeda(i.valor)}</td>
-      </tr>
-    `).join("");
   }
-
 };
