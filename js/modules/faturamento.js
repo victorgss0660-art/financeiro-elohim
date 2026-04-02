009325665e409f373b487da20a07a255750852d0
victorgss0660-art <victorgss0660@gmail.com>
2026-04-02 15:22:27 -0300

    Update faturamento.js

--- js/modules/faturamento.js
+++ js/modules/faturamento.js
@@ -1,15 +1,50 @@
 window.faturamentoModule = {
+  async garantirMes(mes, ano) {
+    const data = await api.restGet(
+      "meses",
+      `select=*&mes=eq.${encodeURIComponent(mes)}&ano=eq.${encodeURIComponent(ano)}&limit=1`
+    );
+
+    if (!data.length) {
+      await api.restInsert("meses", [{
+        mes,
+        ano,
+        faturamento: 0
+      }]);
+    }
+  },
 
   async salvarFaturamento() {
-    const { mes, ano } = utils.getMesAno();
+    try {
+      const { mes, ano } = utils.getMesAno();
+      const input = document.getElementById("faturamentoInput");
 
-    await api.restInsert("faturamento", [{
-      valor: Number(faturamentoInput.value),
-      mes,
-      ano
-    }]);
+      if (!input) {
+        utils.setAppMsg("Campo de faturamento não encontrado.", "err");
+        return;
+      }
 
-    utils.setAppMsg("Faturamento salvo", "ok");
+      const valor = utils.num(input.value || 0);
+
+      await this.garantirMes(mes, ano);
+
+      await api.restPatch(
+        "meses",
+        `mes=eq.${encodeURIComponent(mes)}&ano=eq.${encodeURIComponent(ano)}`,
+        { faturamento: valor }
+      );
+
+      utils.setAppMsg("Faturamento salvo com sucesso.", "ok");
+
+      if (window.dashboardModule?.carregarDashboard) {
+        await window.dashboardModule.carregarDashboard();
+      }
+
+      if (window.resumoModule?.carregarResumoAnual) {
+        await window.resumoModule.carregarResumoAnual();
+      }
+    } catch (e) {
+      utils.setAppMsg("Erro ao salvar faturamento: " + e.message, "err");
+    }
   }
-
 };
